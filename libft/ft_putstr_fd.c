b012ce0876fea724a180d71796b9d2990089e020
J Takahashi <jtakahas@c1r26s2.42tokyo.jp>
2023-09-28 16:52:33 +0900

    delete libft/putstr main fanction

--- libft/ft_putstr_fd.c
+++ libft/ft_putstr_fd.c
@@ -6,7 +6,7 @@
 /*   By: jtakahas <jtakahas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/26 13:53:20 by jtakahas          #+#    #+#             */
-/*   Updated: 2023/09/28 16:49:55 by jtakahas         ###   ########.fr       */
+/*   Updated: 2023/09/28 16:51:21 by jtakahas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,4 @@ void	ft_putstr_fd(char *s, int fd)
 	if (!s)
 		return ;
 	write(fd, &*s, ft_strlen(s));
-}
-
-int main(void)
-{
-	ft_putstr_fd("1234567", 1);
-	return (0);
 }
