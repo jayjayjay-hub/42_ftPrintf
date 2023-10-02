c7094cc47d7097c7497a58dbcc3ec1c8ed46a644
J Takahashi <jtakahas@c1r21s1.42tokyo.jp>
2023-10-02 20:03:57 +0900

    edit uitoa

--- unsigned_itoa.c
+++ unsigned_itoa.c
@@ -6,14 +6,14 @@
 /*   By: jtakahas <jtakahas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/05 15:27:38 by jtakahas          #+#    #+#             */
-/*   Updated: 2023/10/02 19:57:56 by jtakahas         ###   ########.fr       */
+/*   Updated: 2023/10/02 20:02:11 by jtakahas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #include "libft.h"
 
-static char	*ft_to_char(char *str, unsigned int number, int len)
+static void	ft_to_char(char *str, unsigned int number, int len)
 {
 	str[len] = '\0';
 	len--;
@@ -25,7 +25,7 @@ static char	*ft_to_char(char *str, unsigned int number, int len)
 		number /= 10;
 		len--;
 	}
-	return (str);
+	return ;
 }
 
 static int	ft_nbrlen(unsigned int nbr)
@@ -52,6 +52,6 @@ char	*unsigned_itoa(unsigned int nbr)
 	str = (char *)malloc((len_nbr + 1));
 	if (!(str))
 		return (NULL);
-	str = ft_to_char(str, nbr, len_nbr);
+	ft_to_char(str, nbr, len_nbr);
 	return (str);
 }
